4550d8a976cc8aea69db1a45fc8980deae73d71c
Eduardo Padilha do Nascimento <79178314+eduardo-padilha-dev@users.noreply.github.com>
2026-03-28 18:20:47 -0300

    setup: instalar zustand (#43)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { Text, View } from "react-native";
+import { useMedicationStore } from "../../store/useMedicationStore";
 
 export default function HomeScreen() {
+  const { medications } = useMedicationStore();
+
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        NativeWind funcionando! 🎉
+        {medications.length} medicamentos cadastrados
       </Text>
     </View>
   );
