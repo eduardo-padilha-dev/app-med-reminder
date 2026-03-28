c59c0eadc318f121f19383b2d55ae7ccb8a77f0c
Eduardo Padilha <padilhanasc@gmail.com>
2026-03-28 14:17:45 -0300

    setup: instalar e configurar NativeWind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-xl font-bold text-blue-500">
+        NativeWind funcionando! 🎉
+      </Text>
+    </View>
+  );
+}
